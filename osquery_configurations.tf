c8c1fcd62f70e574906d49c790bc6d55457623f3
Dan <dan@redcupit.com>
2024-07-09 13:46:42 -0600

    adding query

--- osquery_configurations.tf
+++ osquery_configurations.tf
@@ -8,3 +8,10 @@ resource "zentral_osquery_enrollment" "default" {
   configuration_id      = zentral_osquery_configuration.default.id
   meta_business_unit_id = zentral_meta_business_unit.default.id
 }
+
+# Pack
+
+resource "zentral_osquery_configuration_pack" "default-compliance-checks" {
+  configuration_id = zentral_osquery_configuration.default.id
+  pack_id          = zentral_osquery_pack.compliance-checks.id
+}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "api_token" {
   type        = string
   sensitive   = true
 }
+
+variable "osquery_default_compliance_check_interval" {
+  description = "Default Osquery compliance check scheduling interval in seconds"
+  default     = 3600
+}
