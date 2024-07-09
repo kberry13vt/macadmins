0a7fc8effdfc36da51ccd4a02dc10b8155b4f423
Dan <dan@redcupit.com>
2024-07-09 16:08:39 -0400

    Update santa_configurations.tf

--- santa_configurations.tf
+++ santa_configurations.tf
@@ -8,3 +8,13 @@ resource "zentral_santa_enrollment" "default" {
   configuration_id      = zentral_santa_configuration.default.id
   meta_business_unit_id = zentral_meta_business_unit.default.id
 }
+
+resource "zentral_santa_rule" "teamid-macpaw" {
+  configuration_id  = zentral_santa_configuration.default.id
+  policy            = "BLOCKLIST"
+  target_type       = "TEAMID"
+  target_identifier = "S8EX82NJP6"
+  custom_message    = "No MacPaw apps are allowed!!!"
+  description       = "Block MacPaw apps, mostly for demo purposes"
+}
+
