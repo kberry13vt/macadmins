b7d4c60fa846ace34e3f5dc44ea9cd3fe8cd0627
Dan <dan@redcupit.com>
2024-07-09 13:59:44 -0400

    ran terraform fmt

--- provider.tf
+++ provider.tf
@@ -5,13 +5,13 @@ terraform {
     }
   }
 
-backend "http" {
-  address        = "https://psumac24-nevada.zentral.cloud/api/terraform/backend/starter_kit/"
-  lock_address   = "https://psumac24-nevada.zentral.cloud/api/terraform/backend/starter_kit/lock/"
-  unlock_address = "https://psumac24-nevada.zentral.cloud/api/terraform/backend/starter_kit/lock/"
-  lock_method    = "POST"
-  unlock_method  = "DELETE"
-	}
+  backend "http" {
+    address        = "https://psumac24-nevada.zentral.cloud/api/terraform/backend/starter_kit/"
+    lock_address   = "https://psumac24-nevada.zentral.cloud/api/terraform/backend/starter_kit/lock/"
+    unlock_address = "https://psumac24-nevada.zentral.cloud/api/terraform/backend/starter_kit/lock/"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+  }
 
 }
 
@@ -19,7 +19,7 @@ backend "http" {
 provider "zentral" {
   // URL where the API endpoints are mounted in the Zentral deployment.
   // The ZTL_API_BASE_URL environment variable can be used instead.
-base_url = "https://psumac24-nevada.zentral.cloud/api/"
+  base_url = "https://psumac24-nevada.zentral.cloud/api/"
 
   // Zentral service account (better) or user API token.
   // This is a secret, it must be managed using a variable.
